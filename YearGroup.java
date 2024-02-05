7100268d22e8a8aa5b721e21db0e53bf90d5d1bd
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2024-02-05 08:47:46 +0100

    YearGroup added

---
+++ YearGroup.java
@@ -0,0 +1,50 @@
+import java.time.LocalDate;
+import java.util.Arrays;
+
+public class YearGroup {
+    private int schoolYear;
+    private LocalDate beginningOfSchoolYear;
+
+    private Student[] students;
+
+    public YearGroup(int schoolYear, LocalDate beginningOfSchoolYear, Student[] students) {
+        this.schoolYear = schoolYear;
+        this.beginningOfSchoolYear = beginningOfSchoolYear;
+        this.students = students;
+    }
+
+    public YearGroup(){}
+
+    public int getSchoolYear() {
+        return schoolYear;
+    }
+
+    public void setSchoolYear(int schoolYear) {
+        this.schoolYear = schoolYear;
+    }
+
+    public LocalDate getBeginningOfSchoolYear() {
+        return beginningOfSchoolYear;
+    }
+
+    public void setBeginningOfSchoolYear(LocalDate beginningOfSchoolYear) {
+        this.beginningOfSchoolYear = beginningOfSchoolYear;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "YearGroup{" +
+                "schoolYear=" + schoolYear +
+                ", beginningOfSchoolYear=" + beginningOfSchoolYear +
+                ", students=" + Arrays.toString(students) +
+                '}';
+    }
+}
